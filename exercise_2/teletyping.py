e7ce50e6d66390784d5bc49d78878434e90926bd
S. Hutchins <ms.s.hutchins@gmail.com>
2018-05-08 15:18:26 -0500

    Added Exercsie 2 folder.

---
+++ exercise_2/teletyping.py
@@ -0,0 +1,63 @@
+# x is a variable
+# it's type is an integer
+x = 5
+y = 2 * x
+
+# you can add integers together like a normal math equation
+z = 2 * (y + x)
+
+# print() is a function
+# functions always have parentheses
+print(y)
+print(z)
+
+# a, b, and c are also variables
+# Their types are strings
+a = 'xiao'
+b = 'rob'
+c = "shaurita"
+d = "snack"
+number = 999
+space = " "
+
+
+# You can also add strings together
+e = a + space + b + space + c + space + d
+
+# Formatted strings
+f = f""
+formatted_string = f"{a} {b} {c} {d} {number}"
+sentence = "I am teletyping with {}, {}, and {}, but I want a {}.".format(a, b, c, d)
+print(e)
+print(formatted_string)
+print(sentence)
+
+# a-d are variables
+# They are types of strings
+a = "tiger"
+b = "monkey"
+c = "lion"
+d = "bear"
+
+# add strings together
+e = a + 2 * space + b + space + c + 2*space
+print(e)
+
+#def print(items):
+#    send_as_output(items)
+
+def get_popular_genes(type):
+    print(type)
+
+get_popular_genes(type="GPCR")
+
+# Math symbols
+# * == multiply
+# / == divide
+# - == subtract
+# + == addition
+
+# print() - is the most popular and important python functions
+# list()
+# dict()
+# dir()
